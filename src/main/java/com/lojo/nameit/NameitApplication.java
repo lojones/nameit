937890f7732c23773ecd3a86eb1400ac2d9fa14b
lojones <login.jones@gmail.com>
2023-06-18 21:11:11 -0400

    genesis

---
+++ src/main/java/com/lojo/nameit/NameitApplication.java
@@ -0,0 +1,13 @@
+package com.lojo.nameit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojo/nameit/NameitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojo.nameit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
