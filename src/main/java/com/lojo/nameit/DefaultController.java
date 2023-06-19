b06f3c338c589fcc0cd5804c56ae0ede59ac7ea6
lojones <login.jones@gmail.com>
2023-06-19 01:01:14 -0400

    genesis 2

---
+++ src/main/java/com/lojo/nameit/DefaultController.java
@@ -0,0 +1,10 @@
+package com.lojo.nameit;
+
+// Make this class a spring controller api
+// Make this class a rest controller
+// Make this class a request mapping for the root path 
+
+
+public class DefaultController {
+    
+}
